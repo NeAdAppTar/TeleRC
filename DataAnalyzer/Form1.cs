ddf5da213283a941b1814283e8bad3adaf34a08b
NeAdAppTar <93933362+NeAdAppTar@users.noreply.github.com>
2024-11-02 20:44:40 +0300

    Update Form1.cs

--- DataAnalyzer/Form1.cs
+++ DataAnalyzer/Form1.cs
@@ -1,225 +1,279 @@
-﻿using System;
+using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.IO.Ports;
 using System.Windows.Forms;
 using LiveCharts;
-using LiveCharts.Wpf;
 using LiveCharts.WinForms;
-using LiveCharts.Defaults;
+using LiveCharts.Wpf;
 
-namespace DataAnalyzer
+namespace TeleRC
 {
     public partial class Form1 : Form
     {
         private SerialPort serialPort;
+        private bool isConnected = false;
 
-        private LineSeries altitudeSeries;
-        private LineSeries accelXSeries;
-        private LineSeries accelYSeries;
-        private LineSeries accelZSeries;
-        private LineSeries accelTotalSeries;
+        // Отдельные серии для каждой оси ускорения
+        private ChartValues<double> accelXValues;
+        private ChartValues<double> accelYValues;
+        private ChartValues<double> accelZValues;
+        private ChartValues<double> altitudeValues;
 
         public Form1()
         {
             InitializeComponent();
-
-            // Настраиваем графики
-            altitudeSeries = new LineSeries
-            {
-                Values = new ChartValues<ObservablePoint>(),
-                Stroke = System.Windows.Media.Brushes.Blue,
-                Fill = System.Windows.Media.Brushes.Transparent,
-                Title = "Линия зависимости"
-            };
-
-            accelXSeries = new LineSeries
-            {
-                Values = new ChartValues<ObservablePoint>(),
-                Stroke = System.Windows.Media.Brushes.Red,
-                Fill = System.Windows.Media.Brushes.Transparent,
-                Title = "X"
-            };
-
-            accelYSeries = new LineSeries
-            {
-                Values = new ChartValues<ObservablePoint>(),
-                Stroke = System.Windows.Media.Brushes.Green,
-                Fill = System.Windows.Media.Brushes.Transparent,
-                Title = "Y"
-            };
-
-            accelZSeries = new LineSeries
-            {
-                Values = new ChartValues<ObservablePoint>(),
-                Stroke = System.Windows.Media.Brushes.Orange,
-                Fill = System.Windows.Media.Brushes.Transparent,
-                Title = "Z"
-            };
-
-            accelTotalSeries = new LineSeries
-            {
-                Values = new ChartValues<ObservablePoint>(),
-                Stroke = System.Windows.Media.Brushes.Purple,
-                Fill = System.Windows.Media.Brushes.Transparent,
-                Title = "Абсолютное ускорение"
-            };
-
-            cartesianChart1.Series.Add(altitudeSeries);
-            cartesianChart2.Series.Add(accelXSeries);
-            cartesianChart2.Series.Add(accelYSeries);
-            cartesianChart2.Series.Add(accelZSeries);
-            cartesianChart2.Series.Add(accelTotalSeries);
-
-            // Настроим оси и заголовки
-            cartesianChart1.AxisX.Add(new Axis { Title = "Time (s)" });
-            cartesianChart1.AxisY.Add(new Axis { Title = "Altitude (m)" });
-            cartesianChart1.LegendLocation = LegendLocation.Right;
-            // cartesianChart1.Title = "Altitude vs Time"; // Удалено, так как свойство Title может отсутствовать
-
-            cartesianChart2.AxisX.Add(new Axis { Title = "Time (s)" });
-            cartesianChart2.AxisY.Add(new Axis { Title = "Acceleration (m/s²)" });
-            cartesianChart2.LegendLocation = LegendLocation.Right;
-            // cartesianChart2.Title = "Acceleration vs Time"; // Удалено, так как свойство Title может отсутствовать
+            LoadAvailablePorts();
+            InitializeCharts();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void LoadAvailablePorts()
         {
-            // Поиск доступных COM-портов
-            comboBoxPorts.Items.AddRange(SerialPort.GetPortNames());
+            string[] ports = SerialPort.GetPortNames();
+            comboBoxPort.Items.AddRange(ports);
+            if (ports.Length > 0)
+            {
+                comboBoxPort.SelectedIndex = 0;
+            }
         }
 
-        private void btnConnect_Click(object sender, EventArgs e)
+        // Добавим новый набор данных для абсолютного ускорения
+        private ChartValues<double> accelAbsValues;
+
+        private void InitializeCharts()
         {
-            if (serialPort == null)
+            altitudeValues = new ChartValues<double>();
+            accelXValues = new ChartValues<double>();
+            accelYValues = new ChartValues<double>();
+            accelZValues = new ChartValues<double>();
+            accelAbsValues = new ChartValues<double>();  // Новая линия для абсолютного ускорения
+
+            // График высоты
+            cartesianChart1.Series = new SeriesCollection
+    {
+        new LineSeries
+        {
+            Title = "Altitude",
+            Values = altitudeValues,
+            StrokeThickness = 2,
+            PointGeometrySize = 5
+        }
+    };
+
+            // График ускорений (три оси + абсолютное ускорение)
+            cartesianChart2.Series = new SeriesCollection
+    {
+        new LineSeries
+        {
+            Title = "Acceleration X",
+            Values = accelXValues,
+            StrokeThickness = 2,
+            PointGeometrySize = 5
+        },
+        new LineSeries
+        {
+            Title = "Acceleration Y",
+            Values = accelYValues,
+            StrokeThickness = 2,
+            PointGeometrySize = 5
+        },
+        new LineSeries
+        {
+            Title = "Acceleration Z",
+            Values = accelZValues,
+            StrokeThickness = 2,
+            PointGeometrySize = 5
+        },
+        new LineSeries
+        {
+            Title = "Acceleration Abs",
+            Values = accelAbsValues,
+            StrokeThickness = 2,
+            PointGeometrySize = 5
+        }
+    };
+
+            // Оси графика высоты
+            cartesianChart1.AxisX.Add(new Axis
             {
-                serialPort = new SerialPort(comboBoxPorts.SelectedItem.ToString(), 9600);
-                serialPort.DataReceived += SerialPort_DataReceived;
-                serialPort.Open();
-                btnConnect.Text = "Отключить";
+                Title = "Time (s)",
+                LabelFormatter = value => TimeSpan.FromSeconds(value).ToString(@"mm\:ss")
+            });
+
+            cartesianChart1.AxisY.Add(new Axis
+            {
+                Title = "Altitude (m)",
+                LabelFormatter = value => value.ToString("0.##")
+            });
+
+            // Оси графика ускорений
+            cartesianChart2.AxisX.Add(new Axis
+            {
+                Title = "Time (s)",
+                LabelFormatter = value => TimeSpan.FromSeconds(value).ToString(@"mm\:ss")
+            });
+
+            cartesianChart2.AxisY.Add(new Axis
+            {
+                Title = "Acceleration (m/s²)",
+                LabelFormatter = value => value.ToString("0.##")
+            });
+        }
+
+        private void UpdateCharts(string data)
+        {
+            string[] values = data.Split(';');
+            if (values.Length >= 6)
+            {
+                double altitude = Convert.ToDouble(values[2]) / 100.0;
+                double accelX = Convert.ToDouble(values[3]) * 9.81 / 100.0;
+                double accelY = Convert.ToDouble(values[4]) * 9.81 / 100.0;
+                double accelZ = Convert.ToDouble(values[5]) * 9.81 / 100.0;
+
+                // Абсолютное ускорение
+                double accelAbs = Math.Sqrt(accelX * accelX + accelY * accelY + accelZ * accelZ);
+
+                altitudeValues.Add(altitude);
+                accelXValues.Add(accelX);
+                accelYValues.Add(accelY);
+                accelZValues.Add(accelZ);
+                accelAbsValues.Add(accelAbs);
+            }
+        }
+
+        private void btnConnectDisconnect_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                try
+                {
+                    
+                    
+                    string selectedPort = comboBoxPort.SelectedItem.ToString();
+                    int baudRate = int.Parse(comboBoxBaudRate.SelectedItem.ToString());
+
+                    serialPort = new SerialPort(selectedPort, baudRate);
+                    serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+                    serialPort.Open();
+                    AddMessage("Подключено к порту " + selectedPort + " со скоростью " + baudRate);
+                    btnConnectDisconnect.Text = "Отключиться";
+                    isConnected = true;
+                }
+                catch (Exception ex)
+                {
+                    // AddMessage("Ошибка при подключении: " + ex.Message);
+                    MessageBox.Show("[C1001] " + "Ошибка при подключении: " + ex.Message, "Ошибка", MessageBoxButtons.OK);
+                }
             }
             else
             {
-                if (serialPort.IsOpen)
+                try
                 {
                     serialPort.Close();
-                    serialPort.Dispose();
-                    serialPort = null;
+                    AddMessage("Отключено.");
+                    btnConnectDisconnect.Text = "Подключиться";
+                    isConnected = false;
                 }
-                btnConnect.Text = "Подключить";
+                catch (Exception ex)
+                {
+                    //AddMessage("Ошибка при отключении: " + ex.Message);
+                    MessageBox.Show("[C1002] " + "Ошибка при подключении: " + ex.Message, "Ошибка", MessageBoxButtons.OK);
+                }
             }
         }
-        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
             try
             {
                 string data = serialPort.ReadLine();
                 this.Invoke(new Action(() =>
                 {
+                    // Обновляем данные на графиках
+                    UpdateCharts(data);
+
+                    // Отображение данных в TextBox
                     txtData.AppendText(data + Environment.NewLine);
-
-                    // Парсинг данных
-                    var parts = data.Split(';');
-                    if (parts.Length == 10)
-                    {
-                        // Чтение данных
-                        float time = float.Parse(parts[1]) / 1000; // время в секундах
-                        float altitude = float.Parse(parts[2]) / 100; // высота в метрах
-                        float accelX = float.Parse(parts[3]) * 9.81f / 100; // акселерометр X
-                        float accelY = float.Parse(parts[4]) * 9.81f / 100; // акселерометр Y
-                        float accelZ = float.Parse(parts[5]) * 9.81f / 100; // акселерометр Z
-
-                        // Суммарное ускорение
-                        float accelMagnitude = (float)Math.Sqrt(accelX * accelX + accelY * accelY + accelZ * accelZ);
-
-                        // Добавление точек на графики
-                        altitudeSeries.Values.Add(new ObservablePoint(time, altitude));
-                        accelXSeries.Values.Add(new ObservablePoint(time, accelX));
-                        accelYSeries.Values.Add(new ObservablePoint(time, accelY));
-                        accelZSeries.Values.Add(new ObservablePoint(time, accelZ));
-                        accelTotalSeries.Values.Add(new ObservablePoint(time, accelMagnitude));
-
-                        // Ограничиваем количество точек на графиках
-                        if (altitudeSeries.Values.Count > 100)
-                        {
-                            altitudeSeries.Values.RemoveAt(0);
-                            accelXSeries.Values.RemoveAt(0);
-                            accelYSeries.Values.RemoveAt(0);
-                            accelZSeries.Values.RemoveAt(0);
-                            accelTotalSeries.Values.RemoveAt(0);
-                        }
-                    }
                 }));
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Ошибка при чтении данных: " + ex.Message);
+                MessageBox.Show("[R1001] " + "Ошибка при получении данных: " + ex.Message, "Ошибка", MessageBoxButtons.OK);
             }
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        private bool isErrorHandled = false;
+
+        private void AddMessage(string message)
         {
-            // Остановим поток и закроем порт перед выходом
-            if (serialPort != null && serialPort.IsOpen)
+            // txtMessages.AppendText(message + Environment.NewLine);
+
+            try
             {
-                serialPort.Close();
-                serialPort.Dispose();
+                txtMessages.AppendText(message + Environment.NewLine);
+
+                isErrorHandled = false;
+            }
+            catch (Exception ex)
+            {
+                if (!isErrorHandled)
+                {
+                    isErrorHandled = true;
+                    MessageBox.Show("[C2001] " + ex.Message, "Ошибка", MessageBoxButtons.OK);
+                    
+                }
+                
             }
         }
 
-        private void btnSaveData_Click(object sender, EventArgs e)
+
+        // Метод для сохранения графика в файл
+        // Метод для сохранения графиков
+        private void btnSaveChart_Click(object sender, EventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog
-            {
-                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
-                Title = "Сохранить данные"
-            };
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG Files|*.png";
+            saveFileDialog.Title = "Сохранить графики как изображение";
+            saveFileDialog.FileName = "chart.png";
 
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                File.WriteAllText(saveFileDialog.FileName, txtData.Text);
-                MessageBox.Show("Данные сохранены!");
+                string fileName = saveFileDialog.FileName;
+
+                // Сохранение первого графика (высота)
+                Bitmap bmp1 = new Bitmap(cartesianChart1.Width, cartesianChart1.Height);
+                cartesianChart1.DrawToBitmap(bmp1, new Rectangle(0, 0, cartesianChart1.Width, cartesianChart1.Height));
+                bmp1.Save(fileName.Replace(".png", "_altitude.png"), ImageFormat.Png);
+
+                // Сохранение второго графика (ускорение)
+                Bitmap bmp2 = new Bitmap(cartesianChart2.Width, cartesianChart2.Height);
+                cartesianChart2.DrawToBitmap(bmp2, new Rectangle(0, 0, cartesianChart2.Width, cartesianChart2.Height));
+                bmp2.Save(fileName.Replace(".png", "_acceleration.png"), ImageFormat.Png);
+
+                AddMessage("Графики успешно сохранены!");
             }
         }
 
-        private void btnSaveCharts_Click(object sender, EventArgs e)
+        // Метод для сохранения данных в файл
+        // Метод для сохранения данных в файл
+        private void btnSaveData_Click(object sender, EventArgs e)
         {
-            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files|*.txt";
+            saveFileDialog.Title = "Сохранить данные";
+            saveFileDialog.FileName = "data.txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                saveFileDialog.Filter = "PNG Image|*.png";
-                saveFileDialog.Title = "Сохранить графики";
+                string fileName = saveFileDialog.FileName;
+                File.WriteAllText(fileName, txtData.Text);  // txtData — это TextBox, где отображаются данные
 
-                // Выбор имени файла для первого графика
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    try
-                    {
-                        // Сохранение первого графика (Altitude vs Time)
-                        string fileName1 = Path.GetFileNameWithoutExtension(saveFileDialog.FileName) + "_Altitude.png";
-                        string filePath1 = Path.Combine(Path.GetDirectoryName(saveFileDialog.FileName), fileName1);
-                        using (Bitmap bitmap1 = new Bitmap(cartesianChart1.Width, cartesianChart1.Height))
-                        {
-                            cartesianChart1.DrawToBitmap(bitmap1, new Rectangle(0, 0, bitmap1.Width, bitmap1.Height));
-                            bitmap1.Save(filePath1);
-                        }
-
-                        // Сохранение второго графика (Acceleration vs Time)
-                        string fileName2 = Path.GetFileNameWithoutExtension(saveFileDialog.FileName) + "_Acceleration.png";
-                        string filePath2 = Path.Combine(Path.GetDirectoryName(saveFileDialog.FileName), fileName2);
-                        using (Bitmap bitmap2 = new Bitmap(cartesianChart2.Width, cartesianChart2.Height))
-                        {
-                            cartesianChart2.DrawToBitmap(bitmap2, new Rectangle(0, 0, bitmap2.Width, bitmap2.Height));
-                            bitmap2.Save(filePath2);
-                        }
-
-                        MessageBox.Show("Графики сохранены!");
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("Ошибка при сохранении графиков: " + ex.Message);
-                    }
-                }
+                AddMessage("Данные успешно сохранены!");
             }
         }
+
+        
+
     }
 }

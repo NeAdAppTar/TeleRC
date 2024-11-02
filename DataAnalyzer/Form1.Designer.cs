a990fcb5b95ade6bb8147168d325cbe8324f5e29
NeAdAppTar <93933362+NeAdAppTar@users.noreply.github.com>
2024-11-02 20:45:19 +0300

    Update Form1.Designer.cs

--- DataAnalyzer/Form1.Designer.cs
+++ DataAnalyzer/Form1.Designer.cs
@@ -1,15 +1,17 @@
-﻿namespace DataAnalyzer
+﻿namespace TeleRC
 {
     partial class Form1
     {
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.ComboBox comboBoxPorts;
-        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox comboBoxBaudRate;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.Button btnConnectDisconnect;
+        private System.Windows.Forms.Button btnSaveData;
+        private System.Windows.Forms.Button btnSaveChart;
+        private System.Windows.Forms.TextBox txtMessages;
         private System.Windows.Forms.TextBox txtData;
         private LiveCharts.WinForms.CartesianChart cartesianChart1;
         private LiveCharts.WinForms.CartesianChart cartesianChart2;
-        private System.Windows.Forms.Button btnSaveData;
-        private System.Windows.Forms.Button btnSaveCharts;
 
         protected override void Dispose(bool disposing)
         {
@@ -22,94 +24,150 @@
 
         private void InitializeComponent()
         {
-            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
-            this.btnConnect = new System.Windows.Forms.Button();
+            this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.btnConnectDisconnect = new System.Windows.Forms.Button();
+            this.btnSaveData = new System.Windows.Forms.Button();
+            this.btnSaveChart = new System.Windows.Forms.Button();
+            this.txtMessages = new System.Windows.Forms.TextBox();
             this.txtData = new System.Windows.Forms.TextBox();
             this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
             this.cartesianChart2 = new LiveCharts.WinForms.CartesianChart();
-            this.btnSaveData = new System.Windows.Forms.Button();
-            this.btnSaveCharts = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // comboBoxPorts
+            // comboBoxBaudRate
             // 
-            this.comboBoxPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxPorts.FormattingEnabled = true;
-            this.comboBoxPorts.Location = new System.Drawing.Point(12, 10);
-            this.comboBoxPorts.Name = "comboBoxPorts";
-            this.comboBoxPorts.Size = new System.Drawing.Size(121, 21);
-            this.comboBoxPorts.TabIndex = 0;
+            this.comboBoxBaudRate.FormattingEnabled = true;
+            this.comboBoxBaudRate.Items.AddRange(new object[] {
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBoxBaudRate.Location = new System.Drawing.Point(20, 20);
+            this.comboBoxBaudRate.Name = "comboBoxBaudRate";
+            this.comboBoxBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxBaudRate.TabIndex = 0;
             // 
-            // btnConnect
+            // comboBoxPort
             // 
-            this.btnConnect.Location = new System.Drawing.Point(139, 10);
-            this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(94, 23);
-            this.btnConnect.TabIndex = 1;
-            this.btnConnect.Text = "Подключить";
-            this.btnConnect.UseVisualStyleBackColor = true;
-            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(160, 20);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPort.TabIndex = 1;
             // 
-            // txtData
+            // btnConnectDisconnect
             // 
-            this.txtData.Location = new System.Drawing.Point(12, 50);
-            this.txtData.Multiline = true;
-            this.txtData.Name = "txtData";
-            this.txtData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtData.Size = new System.Drawing.Size(300, 150);
-            this.txtData.TabIndex = 2;
-            // 
-            // cartesianChart1
-            // 
-            this.cartesianChart1.Location = new System.Drawing.Point(12, 220);
-            this.cartesianChart1.Name = "cartesianChart1";
-            this.cartesianChart1.Size = new System.Drawing.Size(500, 250);
-            this.cartesianChart1.TabIndex = 3;
-            // 
-            // cartesianChart2
-            // 
-            this.cartesianChart2.Location = new System.Drawing.Point(12, 480);
-            this.cartesianChart2.Name = "cartesianChart2";
-            this.cartesianChart2.Size = new System.Drawing.Size(500, 250);
-            this.cartesianChart2.TabIndex = 4;
+            this.btnConnectDisconnect.Location = new System.Drawing.Point(300, 20);
+            this.btnConnectDisconnect.Name = "btnConnectDisconnect";
+            this.btnConnectDisconnect.Size = new System.Drawing.Size(121, 23);
+            this.btnConnectDisconnect.TabIndex = 2;
+            this.btnConnectDisconnect.Text = "Подключиться";
+            this.btnConnectDisconnect.UseVisualStyleBackColor = true;
+            this.btnConnectDisconnect.Click += new System.EventHandler(this.btnConnectDisconnect_Click);
             // 
             // btnSaveData
             // 
-            this.btnSaveData.Location = new System.Drawing.Point(239, 10);
+            this.btnSaveData.Location = new System.Drawing.Point(20, 60);
             this.btnSaveData.Name = "btnSaveData";
-            this.btnSaveData.Size = new System.Drawing.Size(116, 23);
-            this.btnSaveData.TabIndex = 5;
+            this.btnSaveData.Size = new System.Drawing.Size(121, 23);
+            this.btnSaveData.TabIndex = 3;
             this.btnSaveData.Text = "Сохранить данные";
             this.btnSaveData.UseVisualStyleBackColor = true;
             this.btnSaveData.Click += new System.EventHandler(this.btnSaveData_Click);
             // 
-            // btnSaveCharts
+            // btnSaveChart
             // 
-            this.btnSaveCharts.Location = new System.Drawing.Point(361, 10);
-            this.btnSaveCharts.Name = "btnSaveCharts";
-            this.btnSaveCharts.Size = new System.Drawing.Size(116, 23);
-            this.btnSaveCharts.TabIndex = 6;
-            this.btnSaveCharts.Text = "Сохранить графики";
-            this.btnSaveCharts.UseVisualStyleBackColor = true;
-            this.btnSaveCharts.Click += new System.EventHandler(this.btnSaveCharts_Click);
+            this.btnSaveChart.Location = new System.Drawing.Point(160, 60);
+            this.btnSaveChart.Name = "btnSaveChart";
+            this.btnSaveChart.Size = new System.Drawing.Size(121, 23);
+            this.btnSaveChart.TabIndex = 4;
+            this.btnSaveChart.Text = "Сохранить график";
+            this.btnSaveChart.UseVisualStyleBackColor = true;
+            this.btnSaveChart.Click += new System.EventHandler(this.btnSaveChart_Click);
+            // 
+            // txtMessages
+            // 
+            this.txtMessages.Location = new System.Drawing.Point(20, 100);
+            this.txtMessages.Multiline = true;
+            this.txtMessages.Name = "txtMessages";
+            this.txtMessages.ReadOnly = true;
+            this.txtMessages.Size = new System.Drawing.Size(400, 100);
+            this.txtMessages.TabIndex = 5;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(441, 100);
+            this.txtData.Multiline = true;
+            this.txtData.Name = "txtData";
+            this.txtData.ReadOnly = true;
+            this.txtData.Size = new System.Drawing.Size(400, 100);
+            this.txtData.TabIndex = 6;
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cartesianChart1.Location = new System.Drawing.Point(435, 3);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(426, 378);
+            this.cartesianChart1.TabIndex = 7;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // cartesianChart2
+            // 
+            this.cartesianChart2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cartesianChart2.Location = new System.Drawing.Point(3, 3);
+            this.cartesianChart2.Name = "cartesianChart2";
+            this.cartesianChart2.Size = new System.Drawing.Size(426, 378);
+            this.cartesianChart2.TabIndex = 8;
+            this.cartesianChart2.Text = "cartesianChart2";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.cartesianChart1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cartesianChart2, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 217);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(864, 384);
+            this.tableLayoutPanel1.TabIndex = 9;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(534, 741);
-            this.Controls.Add(this.btnSaveCharts);
+            this.ClientSize = new System.Drawing.Size(864, 601);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.comboBoxBaudRate);
+            this.Controls.Add(this.comboBoxPort);
+            this.Controls.Add(this.btnConnectDisconnect);
             this.Controls.Add(this.btnSaveData);
-            this.Controls.Add(this.cartesianChart2);
-            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.btnSaveChart);
+            this.Controls.Add(this.txtMessages);
             this.Controls.Add(this.txtData);
-            this.Controls.Add(this.btnConnect);
-            this.Controls.Add(this.comboBoxPorts);
             this.Name = "Form1";
-            this.Text = "TeleRC";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Telemetry Receiver";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
